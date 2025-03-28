b407fe771beaf2d4aaa811d9335c93cbeb8eca5d
krishx64 <jaiswalkrishmarch21@gmail.com>
2025-03-28 12:10:05 +0530

    piechart added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import { fetchContestData, fetchProblemData } from "./api/codeforces.js";
+import { Chart } from "react-google-charts";
 
 const { result } = await fetchProblemData("jaiswalxkrish");
 let verdictOk = result;
-verdictOk = verdictOk.filter(
-  (problem) => problem.verdict === "SUBMITTED" || problem.verdict === "OK"
-);
+let allVerdict = new Map();
+for (let i = 0; i < verdictOk.length; i++) {
+  allVerdict.get(verdictOk[i].verdict)
+    ? allVerdict.set(
+        verdictOk[i].verdict,
+        allVerdict.get(verdictOk[i].verdict) + 1
+      )
+    : allVerdict.set(verdictOk[i].verdict, 1);
+}
+let allDataTemp = [];
+for (let [value, key] of allVerdict) {
+  allDataTemp.push([key, value]);
+}
+allDataTemp.sort(sortFunction);
+
+function sortFunction(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
+  } else {
+    return a[0] > b[0] ? -1 : 1;
+  }
+}
+let allData = [["Verdict", "Count"]];
+for (let i = 0; i < allDataTemp.length; i++) {
+  allData.push([allDataTemp[i][1], allDataTemp[i][0]]);
+}
+console.log(allDataTemp);
+verdictOk = verdictOk.filter((problem) => problem.verdict === "OK");
 const solvedProblems = new Map();
 verdictOk = verdictOk.filter((problem) => {
   if (solvedProblems.get(problem.problem.name) === problem.problem.contestId) {
-    console.log(problem.problem.contestId, problem.problem.name);
     return false;
   }
   solvedProblems.set(problem.problem.name, problem.problem.contestId);
   return true;
 });
-console.log(verdictOk);
 
 export default function App() {
+  const options = {
+    title: "Submissions",
+  };
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, Nigga!</h1>
       <h2>No. of problems solved in Codeforces: {verdictOk.length}</h2>
+      <Chart
+        chartType="PieChart"
+        data={allData}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
