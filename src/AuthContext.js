f303aeec66b0dff430e0e130b11632c156a7bb61
krishx64 <jaiswalkrishmarch21@gmail.com>
2025-04-24 02:49:46 +0530

    confirm prod ready

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import BASE_URL from "./config";
 
 const AuthContext = createContext();
 
@@ -10,7 +11,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const refreshAccessToken = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/token", {
+        const res = await fetch(`${BASE_URL}/api/token`, {
           method: "POST",
           credentials: "include", // Send cookies
         });
